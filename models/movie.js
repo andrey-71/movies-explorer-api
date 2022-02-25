b506816159ab5c7e0f7656a3e06fb8ffe025893d
andrey-71 <and_go1991@mail.ru>
2022-02-25 16:47:23 +0300

    feat: создать схему фильма, валидировать url

---
+++ models/movie.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const isUrl = require('validator/lib/isURL');
+
+const movieSchema = new mongoose.Schema({
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: url => isUrl(url),
+      message: 'Неправильный формат ссылки фильма',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: url => isUrl(url),
+      message: 'Неправильный формат ссылки трейлера',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: url => isUrl(url),
+      message: 'Неправильный формат ссылки постера',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
