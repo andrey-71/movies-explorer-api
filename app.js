ad6d981de3abc7d5885d27dc2fdbb5f617584ded
Andrey G <and_go1991@mail.ru>
2022-03-04 14:34:23 +0300

    feat: подключить роут регистрации

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const { PORT, DB_ADDRESS } = require('./utils/config');
+const { createUser } = require('./controllers/users');
 
 const app = express();
 // Подключение к БД
 mongoose.connect(DB_ADDRESS, {
   useNewUrlParser: true,
 });
+
+// Сборка данных в JSON-формат
+app.use(bodyParser.json());
+
+// Роут регистрации
+app.post('/signup', signupValidation, createUser);
 
 // Запуск сервера
 app.listen(PORT, () => {
