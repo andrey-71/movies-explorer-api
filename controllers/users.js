c423ac39add00ed375514e192da9c965e8cd36ab
Andrey G <and_go1991@mail.ru>
2022-03-04 14:28:45 +0300

    feat: добавить обработку ошибок в контроллер регистрации

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,28 @@
-const User = require('../models/user');
+// const { NODE_ENV, JWT_SECRET } = process.env;
 const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const BadRequestError = require('../errors/BadRequestError');
+const ConflictError = require('../errors/ConflictError');
 
 // Регистрация пользователя
-module.exports = createUser = (req, res) => {
+module.exports.createUser = (req, res, next) => {
   const { name, email, password } = req.body;
 
   bcrypt.hash(password, 10)
     .then((hash) => User.create({
       name, email, password: hash,
-      }))
+    }))
     .then((user) => res.status(201).send(user.toJSON()))
     .catch((err) => {
-      console.log(err);
-    })
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при регистрации пользователя'));
+      } else if (err.code === 11000) {
+        next(new ConflictError('Пользователь с таким email уже существует'));
+      } else {
+        next(err);
+      }
+    });
 };
+
+// Авторизация пользователя
