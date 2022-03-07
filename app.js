243f5821f22d7a7d99468e593a42b9b7fab7b115
andrey-71 <and_go1991@mail.ru>
2022-03-07 18:05:54 +0300

    feat: создать и подключить авторизационную мидлвару

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const cookieParser = require('cookie-parser');
 const { PORT, DB_ADDRESS } = require('./utils/config');
 const routes = require('./routes');
 const { createUser, loginUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/error-handler');
 
 const app = express();
@@ -15,10 +17,14 @@ mongoose.connect(DB_ADDRESS, {
 
 // Сборка данных в JSON-формат
 app.use(bodyParser.json());
+// Парсер кук
+app.use(cookieParser());
 
 // Роут регистрации
 app.post('/signup', createUser);
 app.post('/signin', loginUser);
+// Мидлвэр авторизации
+app.use(auth);
 // Защищенные роуты
 app.use(routes);
 
---
+++ middlewares/auth.js
@@ -0,0 +1,17 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+module.exports = (req, res, next) => {
+  const token = req.cookies.jwt;
+  let payload;
+
+  try {
+    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
+  } catch (err) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+
+  req.user = payload;
+  next();
+};
