c283d890df2438a1abc0be8fc92b38bce3217894
andrey-71 <and_go1991@mail.ru>
2022-03-16 09:49:43 +0300

    fix: правильно обрабатывать ошибку при обновлении email пользователя на уже существующий

--- controllers/users.js
+++ controllers/users.js
@@ -83,6 +83,8 @@ module.exports.updateUser = (req, res, next) => User.findByIdAndUpdate(
   .catch((err) => {
     if (err.name === 'ValidationError') {
       next(new BadRequestError('Переданы некорректные данные при обновлении данных пользователя'));
+    } else if (err.code === 11000) {
+      next(new ConflictError('Пользователь с таким email уже существует'));
     } else {
       next(err);
     }
