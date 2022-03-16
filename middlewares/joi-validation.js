c953cee6f5aa3052849c9d51f2c37b785381e514
andrey-71 <and_go1991@mail.ru>
2022-03-16 20:48:54 +0300

    fix: сделать поле name обязательным при обновлении информации пользователя

--- middlewares/joi-validation.js
+++ middlewares/joi-validation.js
@@ -19,7 +19,7 @@ module.exports.signinValidation = celebrate({
 
 module.exports.userInfoValidation = celebrate(({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
   }),
 }));
